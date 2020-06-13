c9d56374bbb955eb57c549775d5ce6680af0cf57
vicyota <victorojeda9@gmail.com>
2020-06-12 22:18:03 -0400

    mando commit de Java clase1

---
+++ Clase1/src/clase1/Clase1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase1;
+
+/**
+ *
+ * @author Vic
+ */
+public class Clase1 {
+
+  
+    public static void main(String[] args) {
+       System.out.println("Hola Silvestre");
+       
+       float num1=5.85F;
+       
+       int resultado=Math.round(num1);
+        System.out.println(resultado);
+               
+               
+    }
+    
+}
